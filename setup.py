f30f89586a269c9dd2bdefa9b6653346c5430e51
Marcus LaFerrera <marcus@randomhack.org>
2016-03-23 12:18:14 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.environ['BUILD_LIB'] = '1'
 
 setup(
     name="stoq",
-    version="0.9.10",
+    version="0.9.11",
     author="Marcus LaFerrera",
     author_email="marcus@punchcyber.com",
     description="A framework for simplifying analysis.",
--- stoq-framework/core.py
+++ stoq-framework/core.py
@@ -71,7 +71,7 @@ from bs4 import UnicodeDammit
 from stoq.plugins import StoqPluginManager
 
 
-__version__ = "0.9.10"
+__version__ = "0.9.11"
 
 
 class Stoq(StoqPluginManager):
