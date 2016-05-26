a1ec67bca058d9fddd92d25ca11548fe5a8e90a2
Marcus LaFerrera <marcus@randomhack.org>
2016-05-26 13:46:21 -0400

    Add: Default filenames will contain a .stoq extension

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.environ['BUILD_LIB'] = '1'
 
 setup(
     name="stoq",
-    version="0.9.20",
+    version="0.9.21",
     author="Marcus LaFerrera",
     author_email="marcus@punchcyber.com",
     description="A framework for simplifying analysis.",
