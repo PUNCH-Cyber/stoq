f7870e6f39c1ae216ddfa0183ac567d6de759426
Marcus LaFerrera <marcus@randomhack.org>
2018-05-09 15:40:37 -0400

    Add cli module for console_script

--- stoq/cli.py
+++ stoq/cli.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-#   Copyright 2014-2016 PUNCH Cyber Analytics Group
+#   Copyright 2014-2018 PUNCH Cyber Analytics Group
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -17,19 +14,22 @@
 
 import os
 import sys
+import unittest
 
 from time import sleep
 from pathlib import Path
 
 from argparse import RawDescriptionHelpFormatter, ArgumentParser
 
+import stoq
 from stoq import __version__
 from stoq.core import Stoq
 from stoq.shell import StoqShell
 from stoq.logo import print_logo
+from stoq.plugins import StoqPluginInstaller
 
 
-if __name__ == '__main__':
+def main():
 
     # If $STOQ_HOME exists, set our base directory to that, otherwise
     # use ~/.stoq
@@ -91,7 +91,6 @@ if __name__ == '__main__':
         s.list_plugins()
 
     elif options.command == "install":
-        from stoq.plugins import StoqPluginInstaller
         installer = StoqPluginInstaller(s)
         installer.install()
 
@@ -99,13 +98,10 @@ if __name__ == '__main__':
         StoqShell(s).cmdloop()
 
     elif options.command == "runtests":
-        import os
-        import unittest
         # Use tests from installed $CWD/tests, otherwise, try to use the install stoQ tests
         test_path = os.path.join(os.getcwd(), "tests")
         if not os.path.isdir(test_path):
             try:
-                import stoq
                 test_path = os.path.join(os.path.dirname(stoq.__file__), "tests")
             except ImportError:
                 print("Test suite not found. Is stoQ installed or are tests in {}?".format(test_path))
@@ -119,9 +115,9 @@ if __name__ == '__main__':
     else:
         # Initialize and load the worker plugin and make it an object of our
         # stoq class
-        stoq.log.info("Starting stoQ v{}".format(__version__))
+        s.log.info("Starting stoQ v{}".format(__version__))
 
-        worker = stoq.load_plugin(options.command, 'worker')
+        worker = s.load_plugin(options.command, 'worker')
         if not worker:
             exit(-1)
 
@@ -133,6 +129,3 @@ if __name__ == '__main__':
         else:
             # No cron value was provided, let's run once and exit.
             worker.run()
-
-# Done!
-exit(0)
