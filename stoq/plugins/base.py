806a4f5b6e2afe8984a788fd20c45e4bb9627d78
Marcus LaFerrera <marcus@randomhack.org>
2018-05-12 05:58:06 -0400

    Add license

--- stoq/plugins/base.py
+++ stoq/plugins/base.py
@@ -1,3 +1,17 @@
+#   Copyright 2014-2018 PUNCH Cyber Analytics Group
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 import logging
 
 from pkg_resources import parse_version as version
@@ -36,7 +50,7 @@ class StoqPluginBase:
         if not self.min_version or not self.max_version:
             self.incompatible_plugin = True
             self.log.warning("Plugin not compatible with this version of stoQ. "
-                          "Unpredictable results may occur!")
+                             "Unpredictable results may occur!")
 
         # See if plugin options were provided when Stoq() was instantiated
         plugin_options = self.stoq.plugin_options.get(self.category, {}).get(self.name, {})
